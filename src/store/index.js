9eb86055225f2abdd6683404bab777921966939d
Isha Kansal <ikansal@innow8apps.com>
2021-02-03 16:45:27 +0530

    redux saga implemented

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { applyMiddleware, createStore, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createSagaMiddleware from "redux-saga";
+
+import { rootReducer } from "./reducers/index";
+import rootSaga from "./sagas/index";
+import logger from "redux-logger";
+
+const persistConfig = {
+  debug: false,
+  key: "root",
+  keyPrefix: "v.1",
+  storage,
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  compose(applyMiddleware(sagaMiddleware, logger))
+);
+export const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+export default { store, persistor };
