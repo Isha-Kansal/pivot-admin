f620d0b37b32eef18c435c1564625dcdbd41ecbb
Isha Kansal <ikansal@innow8apps.com>
2021-02-04 15:13:58 +0530

    breadcrumb added

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import { withRouter } from "react-router-dom";
-import { CHeader, CHeaderNav } from "@coreui/react";
+import {
+  CHeader,
+  CHeaderNav,
+  CSubheader,
+  CBreadcrumbRouter,
+} from "@coreui/react";
 
-// routes config
-
+import routes from "../routes";
 import { TheHeaderDropdown } from "./index";
 
 const TheHeader = (props) => {
@@ -15,6 +19,12 @@ const TheHeader = (props) => {
       <CHeaderNav className="px-3">
         <TheHeaderDropdown history={props.history} />
       </CHeaderNav>
+      <CSubheader className="px-3 justify-content-between">
+        <CBreadcrumbRouter
+          className="border-0 c-subheader-nav m-0 px-0 px-md-3"
+          routes={routes}
+        />
+      </CSubheader>
     </CHeader>
   );
 };
