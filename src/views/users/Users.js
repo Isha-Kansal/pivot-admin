0895fb116cd0c02ca5dbdf9d36207700ff90f009
Isha Kansal <ikansal@innow8apps.com>
2021-02-12 16:19:56 +0530

    pagination work

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -3,6 +3,7 @@ import { useHistory, useLocation } from "react-router-dom";
 import Pagination from "react-js-pagination";
 import CommonModal from "../../common/commonModal";
 import { NotificationManager } from "react-notifications";
+import { Table, Card, CardHeader, Button } from "reactstrap";
 import {
   CBadge,
   CButton,
@@ -23,6 +24,7 @@ import usersData from "./UsersData";
 import { fetchUsers, userStatus } from "../store/action";
 
 const Users = (props) => {
+  const offsetLimit = 10;
   const history = useHistory();
   const queryPage = useLocation().search.match(/page=([0-9]+)/, "");
   const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1);
@@ -34,9 +36,22 @@ const Users = (props) => {
   const [idUser, setIdUser] = useState("");
   const [usersDetails, setUsersDetails] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [count, setCount] = useState(0);
   const pageChange = (newPage) => {
+    console.log("498568459689458978", newPage);
     currentPage !== newPage && history.push(`/users?page=${newPage}`);
+    setLoading(true);
+
+    let limit = 10;
+    props.fetchUsers(
+      `user/all?offset=${newPage}&limit=${limit}&search=${search}`,
+      (value) => {
+        setLoading(false);
+        setUsersDetails(value.data.users);
+        setCount(value.data.count);
+        setPage(newPage);
+      }
+    );
   };
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -47,7 +62,7 @@ const Users = (props) => {
 
   useEffect(() => {
     callApiToFetchAllUsers();
-  }, []);
+  }, [search]);
 
   const callApiToFetchAllUsers = () => {
     setLoading(true);
@@ -58,6 +73,8 @@ const Users = (props) => {
       (value) => {
         setLoading(false);
         setUsersDetails(value.data.users);
+        setCount(value.data.count);
+        // setPage(newPage);
       }
     );
   };
@@ -95,7 +112,7 @@ const Users = (props) => {
         return "primary";
     }
   };
-  const searchRecords = filterRecords();
+  // const searchRecords = filterRecords();
 
   const onBlock = (e, type, item) => {
     setIdUser(item._id);
@@ -128,189 +145,148 @@ const Users = (props) => {
       }
     });
   };
-  if (searchRecords.length === 0) {
-    return <h3 className="text-center no-user-found">No Users Found!</h3>;
-  } else {
-    return (
-      <CRow>
-        <CCol xl={12}>
-          <form>
-            <div className="text-center search-input">
-              <input
-                type="search"
-                className="form-control"
-                placeholder="Search"
-                required
-                onChange={handleSearch}
-              />
+  // if (usersDetails.length === 0) {
+  //   return <h3 className="text-center no-user-found">No Users Found!</h3>;
+  // } else {
+  console.log("4958698459867945789", usersDetails, count);
+  return (
+    <CRow>
+      <CCol xl={12}>
+        <form>
+          <div className="text-center search-input">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Search"
+              required
+              onChange={handleSearch}
+            />
+          </div>
+        </form>
+      </CCol>
+
+      <CCol xl={12}>
+        <CCard className="position-relative">
+          {loading && <Loader />}
+          <CCardBody>
+            <Table className="table" style={{ minHeight: "150px" }}>
+              <thead>
+                <tr>
+                  <th className="text-nowrap ">Name</th>
+
+                  <th>Email</th>
+                  <th>Status</th>
+
+                  <th>Country</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {usersDetails && usersDetails.length === 0 && (
+                  <h3 className="text-center no-user-found">No Users Found!</h3>
+                )}
+                {usersDetails &&
+                  usersDetails.length > 0 &&
+                  usersDetails.map((item, index) => {
+                    return (
+                      <tr
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          history.push({
+                            pathname: `/users/${item._id}`,
+                            state: usersDetails,
+                          })
+                        }
+                      >
+                        <td>
+                          {" "}
+                          {item.first_name && item.last_name
+                            ? item.first_name + " " + item.last_name
+                            : "-"}
+                        </td>
+                        <td>{item.email ? item.email : "-"}</td>
+                        <td>
+                          <CBadge
+                            color={getBadge(
+                              !item.is_verified ? "Not Verified" : "Verified"
+                            )}
+                          >
+                            {!item.is_verified ? "Not Verified" : "Verified"}
+                          </CBadge>
+                        </td>
+                        <td>{item.country ? item.country : "-"}</td>
+                        <td>
+                          {item.status === "blocked" ? (
+                            <CButton
+                              onClick={(e) => onBlock(e, "unblock", item)}
+                              className="Unblock-btn block-btn"
+                            >
+                              UnBlock
+                            </CButton>
+                          ) : item.status === "activated" ? (
+                            <div>
+                              <CButton
+                                onClick={(e) => onBlock(e, "block", item)}
+                                className="block-btn block-btn"
+                              >
+                                Block
+                              </CButton>{" "}
+                            </div>
+                          ) : item.status === "deactivated" ? (
+                            <div>
+                              <CButton
+                                onClick={(e) => onBlock(e, "block", item)}
+                                className="block-btn block-btn"
+                              >
+                                Block
+                              </CButton>
+                            </div>
+                          ) : (
+                            <CButton
+                              onClick={(e) => onBlock(e, "block", item)}
+                              className="block-btn block-btn"
+                            >
+                              Block
+                            </CButton>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+
+            <div className="text-center pagination-input">
+              {count > 10 && (
+                <Pagination
+                  className="mt-3 mx-auto w-fit-content"
+                  itemClass="page-item"
+                  linkClass="page-link"
+                  activeClass="active"
+                  activePage={page}
+                  itemsCountPerPage={offsetLimit}
+                  totalItemsCount={count}
+                  pageRangeDisplayed={5}
+                  onChange={pageChange}
+                />
+              )}
             </div>
-          </form>
-        </CCol>
-
-        <CCol xl={12}>
-          <CCard className="position-relative">
-            {/* <CCardHeader>Users</CCardHeader> */}
-            {loading && <Loader />}
-            <CCardBody>
-              <CDataTable
-                items={searchRecords}
-                fields={[
-                  { key: "name", _classes: "font-weight-bold" },
-                  "email",
-                  "status",
-                  "country",
-                  "action",
-                ]}
-                scopedSlots={{
-                  name: (item) => (
-                    <td>
-                      {item.first_name && item.last_name
-                        ? item.first_name + " " + item.last_name
-                        : "-"}
-                    </td>
-                  ),
-                  email: (item) => <td>{item.email ? item.email : "-"}</td>,
-                  status: (item) => (
-                    <td>
-                      <CBadge
-                        color={getBadge(
-                          !item.is_verified ? "Not Verified" : "Verified"
-                        )}
-                      >
-                        {!item.is_verified ? "Not Verified" : "Verified"}
-                      </CBadge>
-                    </td>
-                  ),
-                  country: (item) => (
-                    <td>{item.country ? item.country : "-"}</td>
-                  ),
-                  action: (item) => (
-                    <td>
-                      {item.status === "blocked" ? (
-                        <CButton
-                          onClick={(e) => onBlock(e, "unblock", item)}
-                          className="Unblock-btn block-btn"
-                        >
-                          UnBlock
-                        </CButton>
-                      ) : item.status === "activated" ? (
-                        <div>
-                          <CButton
-                            onClick={(e) => onBlock(e, "block", item)}
-                            className="block-btn block-btn"
-                          >
-                            Block
-                          </CButton>{" "}
-                          {/* <CButton
-                          onClick={(e) => onBlock(e, "deactivate", item)}
-                          className="Deactive-btn block-btn"
-                        >
-                          Deactivate
-                        </CButton> */}
-                        </div>
-                      ) : item.status === "deactivated" ? (
-                        <div>
-                          {/* <CButton
-                          onClick={(e) => onBlock(e, "activate", item)}
-                          className="Unblock-btn block-btn"
-                        >
-                          Activate
-                        </CButton> */}
-                          <CButton
-                            onClick={(e) => onBlock(e, "block", item)}
-                            className="block-btn block-btn"
-                          >
-                            Block
-                          </CButton>
-                        </div>
-                      ) : (
-                        <CButton
-                          onClick={(e) => onBlock(e, "block", item)}
-                          className="block-btn block-btn"
-                        >
-                          Block
-                        </CButton>
-                      )}
-
-                      {/* {item.status !== "blocked" && (
-                      <CButton
-                        onClick={(e) => onBlock(e, "block", item)}
-                        className="block-btn block-btn"
-                      >
-                        Block
-                      </CButton>
-                    )}
-                    {item.status === "blocked" && (
-                      <CButton
-                        onClick={(e) => onBlock(e, "unblock", item)}
-                        className="Unblock-btn block-btn"
-                      >
-                        UnBlock
-                      </CButton>
-                    )}
-
-                    {item.status !== "inactivated" && (
-                      <CButton
-                        onClick={(e) => onBlock(e, "deactivate")}
-                        className="Deactive-btn block-btn"
-                      >
-                        Deactivate
-                      </CButton>
-                    )}
-                    {item.status === "inactivated" && (
-                      <CButton
-                        onClick={(e) => onBlock(e, "activate")}
-                        className="Unblock-btn block-btn"
-                      >
-                        Activate
-                      </CButton>
-                    )} */}
-                    </td>
-                  ),
-                }}
-                itemsPerPage={10}
-                activePage={page}
-                clickableRows
-                onRowClick={(item) =>
-                  history.push({
-                    pathname: `/users/${item._id}`,
-                    state: usersDetails,
-                  })
-                }
-              />
-
-              <div className="text-center pagination-input">
-                {usersDetails.length > 10 && (
-                  <Pagination
-                    className="mt-3 mx-auto w-fit-content"
-                    itemClass="page-item"
-                    linkClass="page-link"
-                    activeClass="active"
-                    activePage={page}
-                    itemsCountPerPage={10}
-                    totalItemsCount={usersDetails.length}
-                    pageRangeDisplayed={5}
-                    onChange={pageChange}
-                  />
-                )}
-              </div>
-              <div>
-                {modalOpen && (
-                  <CommonModal
-                    isOpen={modalOpen}
-                    toggle={(e) => onBlock(e, type, idUser)}
-                    blockUser={(e) => blockUser(e, idUser)}
-                    id={idUser}
-                    type={type}
-                  />
-                )}
-              </div>
-            </CCardBody>
-          </CCard>
-        </CCol>
-      </CRow>
-    );
-  }
+            <div>
+              {modalOpen && (
+                <CommonModal
+                  isOpen={modalOpen}
+                  toggle={(e) => onBlock(e, type, idUser)}
+                  blockUser={(e) => blockUser(e, idUser)}
+                  id={idUser}
+                  type={type}
+                />
+              )}
+            </div>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  );
 };
 
 const mapStateToProps = (state) => {
