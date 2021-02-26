7cb9f89c207142a945c1440559fd5c5560b39c3c
Isha Kansal <ikansal@innow8apps.com>
2021-02-26 12:06:42 +0530

    extra data removed

--- src/views/users/UserPlanner.js
+++ src/views/users/UserPlanner.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Loader from "../../loader";
 import {
   CCard,
@@ -6,29 +6,23 @@ import {
   CCardHeader,
   CCol,
   CRow,
-  CBadge,
   CTabs,
   CNavItem,
   CNavLink,
   CNav,
   CTabContent,
   CTabPane,
-  CButton,
-  CCollapse,
 } from "@coreui/react";
-import { Table } from "reactstrap";
+
 import Network from "./planner/Network";
 import Explore from "./planner/Explore";
 import Learn from "./planner/Learn";
 import Prepare from "./planner/Prepare";
 import Apply from "./planner/Apply";
-const UserPlanner = (props) => {
-  const [loading, setLoading] = useState(false);
-
+const UserPlanner = () => {
   return (
     <CRow>
       <CCol lg={12}>
-        {loading && <Loader />}
         <CCard className="position-relative">
           <CCardHeader>
             Click on a module below to see the information
