52bfacc2dd1c8eda740432cf5d63fc29605344b5
Isha Kansal <ikansal@innow8apps.com>
2021-03-18 12:48:37 +0530

    logger

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./reducers/index";
 import rootSaga from "./sagas/index";
 import logger from "redux-logger";
-
+import { isProduction } from "../config";
 const persistConfig = {
   debug: false,
   key: "root",
@@ -18,10 +18,18 @@ const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(
-  persistedReducer,
-  compose(applyMiddleware(sagaMiddleware, logger))
-);
+// export const store = createStore(
+//   persistedReducer,
+//   compose(applyMiddleware(sagaMiddleware, logger))
+// );
+
+export const store = isProduction
+  ? createStore(persistedReducer, compose(applyMiddleware(sagaMiddleware)))
+  : createStore(
+      persistedReducer,
+      compose(applyMiddleware(sagaMiddleware, logger))
+    );
+
 export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
