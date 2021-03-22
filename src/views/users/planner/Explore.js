88cc0c82460dd19aab2f06cf9b07da5dae7e2d54
Isha Kansal <ikansal@innow8apps.com>
2021-03-22 17:53:31 +0530

    colors added

--- src/views/users/planner/Explore.js
+++ src/views/users/planner/Explore.js
@@ -24,7 +24,7 @@ const Explore = (props) => {
   const getBadge = (status) => {
     switch (status) {
       case "Done":
-        return "success";
+        return "green";
 
       // case "Not Verified":
       //   return "danger";
